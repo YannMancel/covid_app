926dfd07484d232b02798efa236f80ae658dca88
Yann Mancel <yann.mancel@hotmail.fr>
2020-11-22 11:59:34 +0100

    Set Orientation in Portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:covid_app/ui_layer/root_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(RootApp());
+void main() {
+  // Just in Portrait mode
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  runApp(RootApp());
+}
